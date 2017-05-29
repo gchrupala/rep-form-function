8aaf6d682daf00bbdc0c05a2dbf28c746bbe2a9d
Grzegorz Chrupała <pitekus@gmail.com>
2017-05-29 13:35:05 +0200

    Git more messing with section 5.

--- src/analysis.R
+++ src/analysis.R
@@ -4,20 +4,24 @@ library(ggplot2)
 dep <- read.csv("../data/depparse_coco_val.csv", header=TRUE)
 omi <- read.csv("../data/omission_coco_val.csv", header=TRUE)
 
-omi_v  <- omi %>% select(-omission_t, -omission_lm) %>% rename(score=omission_v) %>% mutate(pathway="visual")
-omi_t  <- omi %>% select(-omission_v, -omission_lm) %>% rename(score=omission_t) %>% mutate(pathway="textual")
-omi_lm <- omi %>% select(-omission_t, -omission_v)  %>% rename(score=omission_lm) %>% mutate(pathway="lm")
+omi_v  <- omi %>% select(-omission_t, -omission_lm, -omission_sum) %>% rename(score=omission_v) %>% mutate(pathway="visual")
+omi_t  <- omi %>% select(-omission_v, -omission_lm, -omission_sum) %>% rename(score=omission_t) %>% mutate(pathway="textual")
+omi_lm <- omi %>% select(-omission_t, -omission_v, -omission_sum)  %>% rename(score=omission_lm) %>% mutate(pathway="lm")
+omi_sum <- omi %>% select(-omission_v, -omission_t, -omission_lm)  %>% rename(score=omission_sum) %>% mutate(pathway="sum")
+
+
 
 data_v <- merge(dep, omi_v) 
 data_t <- merge(dep, omi_t) 
 data_lm <- merge(dep, omi_lm)
+data_sum <- merge(dep, omi_sum)
 
 dep_lev <- (data_v %>% group_by(dep) %>% summarize(median_score=median(score)) %>% 
               data.frame %>% arrange(median_score))$dep 
 postag_lev <- (data_v %>% group_by(postag) %>% summarize(median_score=median(score)) %>%
               data.frame %>% arrange(median_score))$postag
 
-data <- rbind(data_t, data_v) %>% rbind(data_lm) %>% 
+data <- rbind(data_t, data_v) %>% rbind(data_lm) %>% rbind(data_sum) %>%
   mutate(dep = factor(dep, levels=dep_lev), 
          postag = factor(postag, levels=postag_lev))
 
@@ -27,8 +31,23 @@ filter_by_count <- function(dat, col, count) {
 MINCOUNT<-500
 
 
-# FIGURE 2
-ggplot(filter_by_count(data, "postag", MINCOUNT)) +
+# figure fig:omissionex
+labels <- (data %>% filter(sentid==69649, pathway=='visual') %>% arrange(position))$word
+ggplot(data %>% filter(sentid==69649, pathway!="sum"), aes(x=position, y=score, color=pathway)) +
+  geom_line() +
+  scale_x_continuous(breaks=0:(length(labels)-1), labels=labels) +
+  xlab("") +
+  theme(legend.title=element_blank()) +
+  theme(aspect.ratio=2/3, text=element_text(size=25)) +
+  theme(axis.text.x=element_text(angle=45, hjust=1))
+ggsave("../doc/omissionex.pdf", width=12, height=10)
+
+
+
+
+
+# FIGURE fig:omission-imaginet
+ggplot(data %>% filter(pathway != "sum") %>% filter_by_count("postag", MINCOUNT)) +
   aes(x=postag, y=score, color=pathway) +
   geom_boxplot(outlier.colour = 'grey', outlier.size = 1, varwidth = FALSE) +
   coord_flip() +
@@ -38,7 +57,7 @@ ggplot(filter_by_count(data, "postag", MINCOUNT)) +
          width=5, height=5)
    
 
-ggplot(filter_by_count(data, "dep", MINCOUNT)) +
+ggplot(data %>% filter(pathway != "sum") %>% filter_by_count("dep", MINCOUNT)) +
        aes(x=dep, y=score, color=pathway) +
   geom_boxplot(outlier.colour = 'grey', outlier.size = 1, varwidth = FALSE) +
   coord_flip() +
@@ -48,7 +67,7 @@ ggplot(filter_by_count(data, "dep", MINCOUNT)) +
          width=5, height=5)
 
 
-# FIGURE 3
+# FIGURE fig:omission-imaginet-ratio
 ratio <- filter(data, pathway=='visual')$score / filter(data, pathway=='textual')$score
 data_r <- filter(data, pathway=="visual") %>% mutate(ratio=ratio, 
                                                     postag=reorder(postag, ratio, median),
