2724526658213c8fb4278a7c909119bb66f36fb1
Grzegorz Chrupała <pitekus@gmail.com>
2017-07-17 11:16:28 +0200

    Script to make zip file with Latex sources.

---
+++ doc/makezip.sh
@@ -0,0 +1,2 @@
+zip source.zip *.pdf *.png *.jpg paper.tex double_paper.tex intro.tex related.tex models.tex experiments.tex categories.tex dimensions.tex conclusions.tex refs.bib clv3.cls compling.bst
+
