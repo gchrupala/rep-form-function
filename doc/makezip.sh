679b77df623f471f6296de832b60eac40ca6541d
Grzegorz Chrupała <pitekus@gmail.com>
2017-07-17 11:46:32 +0200

    copyright makezip

--- doc/makezip.sh
+++ doc/makezip.sh
@@ -1,2 +1,2 @@
-zip source.zip *.pdf *.png *.jpg paper.tex double_paper.tex intro.tex related.tex models.tex experiments.tex categories.tex dimensions.tex conclusions.tex refs.bib clv3.cls compling.bst
+zip source.zip *.png *.jpg imaginet.pdf bootstrappedMI2.pdf raw_mutual.pdf position-new.pdf position-coef.pdf omissionex.pdf top_words.pdf bootstrappedMI.pdf paper.tex double_paper.tex intro.tex related.tex models.tex experiments.tex categories.tex dimensions.tex conclusions.tex refs.bib clv3.cls compling.bst
 
